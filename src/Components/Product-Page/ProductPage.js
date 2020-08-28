5dfa887bf1ea642e822ff0a0a58ee0fc397b0bcc
Rebisha Pokharel <rebisha67@gmail.com>
2020-08-28 14:29:35 +1000

    - updated the way sorting is done

--- src/Components/Product-Page/ProductPage.js
+++ src/Components/Product-Page/ProductPage.js
@@ -7,7 +7,6 @@ import styles from "./productPage.scss";
 
 const ProductPage = () => {
   const [product, setProduct] = useState([]);
-  const [sortProduct, setSortProduct] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,12 +24,15 @@ const ProductPage = () => {
   }, []);
 
   const handleChange = e => {
-    setSortProduct(e.target.value);
     switch (e.target.value) {
       case "lowest":
-        return setProduct(product.sort((a, b) => a.salePrice - b.salePrice));
+        return setProduct(
+          [...product].sort((a, b) => a.salePrice - b.salePrice)
+        );
       case "highest":
-        return setProduct(product.sort((a, b) => b.salePrice - a.salePrice));
+        return setProduct(
+          [...product].sort((a, b) => b.salePrice - a.salePrice)
+        );
       default:
         return product;
     }
